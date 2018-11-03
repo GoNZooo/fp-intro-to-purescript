874d23b97d67ddce945d5f3981cbe273c82de129
Rickard Andersson <gonz@severnatazvezda.com>
2018-11-03 17:09:19 +0200

    first commit

---
+++ src/Helpers.purs
@@ -0,0 +1,16 @@
+module Helpers
+  (undefined, error, todo)
+where
+
+import Prelude (unit, (<>))
+import Unsafe.Coerce (unsafeCoerce)
+import Effect.Exception.Unsafe (unsafeThrow)
+
+undefined :: forall a. a
+undefined = unsafeCoerce unit
+
+error :: forall a. String -> a
+error message = unsafeThrow message
+
+todo :: forall a. String -> a
+todo functionName = error ("TODO: Make `" <> functionName <> "`")
