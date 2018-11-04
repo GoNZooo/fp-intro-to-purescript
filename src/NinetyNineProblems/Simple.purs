5d52ed7a44096962180a47c4ddedcfe698463f50
Rickard Andersson <gonz@severnatazvezda.com>
2018-11-04 17:27:41 +0200

    add more comments to `Simple` module

--- src/NinetyNineProblems/Simple.purs
+++ src/NinetyNineProblems/Simple.purs
@@ -4,6 +4,16 @@ import Data.Int (even)
 import NinetyNineProblems.Helpers (undefined)
 import Prelude (gcd, negate, otherwise, (*), (+), (-), (<<<), (==), (>), (>=))
 
+-- Programming is mostly about applying functions to values and getting some
+-- kind of result. We don't have to apply only one function to something, but
+-- can specify a series of functions to apply to a value, or sometimes apply
+-- one function *or* the other.
+--
+-- In order to get familiar with how PureScript allows you to translate this
+-- concept into code, this module serves as an introduction to how one applies
+-- functions to values and how we specify a function's type, as well as how
+-- this affects things like not supplying every value to the function.
+
 -- | Return the addition of two numbers A and B.
 -- | The type signature (after `::`) says that we take an integer as a first
 -- | and second argument. The thing after the last arrow says what the function
@@ -27,6 +37,18 @@ addAgain = (+)
 -- | functions between arguments.
 addAgain' :: Int -> Int -> Int
 addAgain' a b = a `add` b
+
+-- | When we don't supply all arguments to a function we effectively get what
+-- | is left of the type signature. An example:
+-- | `add5 6` = 11
+-- | add :: Int -> Int -> Int
+-- | add5 ::       Int -> Int
+-- | Note the relation between us giving the first argument and in return
+-- | getting a function representing the rest of the computation.
+-- | Don't worry if this isn't immediately clear right away, as it's something
+-- | you're likely to get familiar with fairly soon.
+add5 :: Int -> Int
+add5 = add 5
 
 -- | Return the sutraction of two numbers A and B.
 subtract :: Int -> Int -> Int
@@ -98,6 +120,10 @@ multiplyWithEvenCommonDivisor x =
       x
 
 -- | Apply a function F to a value A.
+-- | When we write `(a -> b)` this denotes a function that takes an `a` and
+-- | returns a `b`. The signature of this function says that `apply` is a
+-- | function that takes a function from `a` to `b` and a value of type `a`
+-- | and returns a value of type `b`.
 apply :: forall a b. (a -> b) -> a -> b
 apply = undefined
 
@@ -106,8 +132,22 @@ applyTwice :: forall a. (a -> a) -> a -> a
 applyTwice = undefined
 
 -- | Apply the function F after applying the function G to a value A.
+-- | Try the following usage:
+-- | > add5 = applyFAndG (_ + 3) (_ + 2)
+-- | > add5 6
+-- | What is the result?
+-- | Note the type signature and that you've supplied both F and G, but nothing
+-- | representing the value `a`.
+-- | applyFAndG :: forall a b c. (b -> c) -> (a -> b) -> a -> c
+-- | applyFAndG ::                   f    ->     g    -> (a -> c)
+-- | What is returned when you *partially* apply a function like this is the
+-- | rest of the type signature, in this case `a -> c` or `(a -> c)`, which
+-- | represents a function that takes an `a` and returns a `c`. The returned
+-- | result is therefore a function from `a` to `c`.
+-- | This is a very common way to combine two or more functions in order to
+-- | create a new one.
 applyFAndG :: forall a b c. (b -> c) -> (a -> b) -> a -> c
-applyFAndG = undefined
+applyFAndG f g x = f (g x)
 
 -- | Return the subtraction of a Number A and B, but where the function F
 -- | is run on the number B first.
