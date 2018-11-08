784b9b7545b1daa0c471fce2e50c45ffd2703b06
Rickard Andersson <gonz@severnatazvezda.com>
2018-11-08 16:09:35 +0200

    remove defs for `Bool`

--- src/NinetyNineProblems/Bool.purs
+++ src/NinetyNineProblems/Bool.purs
@@ -1,5 +1,8 @@
 module NinetyNineProblems.Bool where
 
+import Prelude (class Show)
+import NinetyNineProblems.Helpers (undefined)
+
 data Bool =
   True
   | False
@@ -7,31 +10,31 @@ data Bool =
 -- | Create a function that takes a boolean value B. If the value is `True`,
 -- | return `False` and if it's `False`, return `True`.
 not :: Bool -> Bool
-not True = False
-not False = True
+not = undefined
 
 -- | Create a function that takes two boolean values A and B. If both are `True`
 -- | return `True`, else return `False`.
 and :: Bool -> Bool -> Bool
-and True b = b
-and _ _ = False
+and = undefined
 
 -- | Create a function that takes two boolean values A and B. If either is `True`
 -- | return `True`, else return `False`.
 or :: Bool -> Bool -> Bool
-or False b = b
-or _ _ = True
+or = undefined
 
 -- | Create a function that takes two boolean values A and B. If both are `True`,
 -- | return `False`, otherwise return `True`.
 nand :: Bool -> Bool -> Bool
-nand True True = False
-nand _ _ = False
+nand = undefined
 
 -- | Create a function that takes two boolean values A and B. If only one of
 -- | them is `True`, return `True`. If both are either `True` or `False`, return
 -- | `False`.
 xor :: Bool -> Bool -> Bool
-xor True b = not b
-xor b True = not b
-xor False b = b
+xor = undefined
+
+--------------------------------------------------------------------------------
+instance showBool :: Show Bool where
+  show True = "True"
+  show False = "False"
+--------------------------------------------------------------------------------
