8c7a2234520ca12b2a6b83db8e22df940a09a479
Rickard Andersson <gonz@severnatazvezda.com>
2018-11-10 11:35:34 +0200

    add docs to `Recursion`

--- src/NinetyNineProblems/Recursion.purs
+++ src/NinetyNineProblems/Recursion.purs
@@ -1,10 +1,25 @@
+-- | Recursion is a central concept in functional programming and it represents
+-- | breaking down a problem into a repeatable smaller step that, when performed
+-- | several times will accomplish a bigger task.
+-- |
+-- | This module should serve as a small introduction to recursion and is meant
+-- | to not tie this excessively into the idea of data and its internal
+-- | structure.
 module NinetyNineProblems.Recursion where
 
 import NinetyNineProblems.Helpers (undefined)
 import Prelude ((+), (-), (*), otherwise, (>))
 
+-- | Sum all the numbers up to and including an integer N, starting at 1.
+-- | ```
+-- | sumZeroToN 5 = 15
+-- | ```
 sumZeroToN :: Int -> Int
 sumZeroToN = undefined
 
+-- | Multiply all the numbers up to and including an integer N, starting at 1.
+-- | ```
+-- | multiplyOneToN 5 = 120
+-- | ```
 multiplyOneToN :: Int -> Int
 multiplyOneToN = undefined
