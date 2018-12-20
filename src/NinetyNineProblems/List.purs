8c7cd9bd93927ef82066b5e991fd3cacc489d0d4
Rickard Andersson <gonz@severnatazvezda.com>
2018-12-21 00:33:32 +0200

    change to unicode forall

--- src/NinetyNineProblems/List.purs
+++ src/NinetyNineProblems/List.purs
@@ -27,13 +27,13 @@ import Prelude (class Eq, const, otherwise, (+), (-), (<<<), (<=), (<>), (==), (
 
 -- | Return the first element (the head) of a list, maybe returning a result
 -- | depending on whether or not there is one element or more in the list.
-head :: forall a. List a -> Maybe a
+head :: ∀ a. List a -> Maybe a
 head Nil = Nothing
 head (x : _) = Just x
 
 -- | Return everything except the head of the list (the tail), maybe returning
 -- | a result depending on whether or not there are any elements in the list.
-tail :: forall a. List a -> Maybe (List a)
+tail :: ∀ a. List a -> Maybe (List a)
 tail Nil = Nothing
 tail (_ : xs) = Just xs
 
@@ -49,40 +49,40 @@ tail (_ : xs) = Just xs
 
 -- | Find the second element of a list, maybe returning a result depending on
 -- | whether or not there are two elements or more in the list.
-second :: forall a. List a -> Maybe a
+second :: ∀ a. List a -> Maybe a
 second = undefined
 
 -- | Find the last element of a list, maybe returning a result depending on
 -- | whether or not there is one element or more in the list.
-last :: forall a. List a -> Maybe a
+last :: ∀ a. List a -> Maybe a
 last = undefined
 
 -- | Find the second last element of a list, maybe returning a result
 -- | depending on whether or not there are two or more elements in the list.
-secondLast :: forall a. List a -> Maybe a
+secondLast :: ∀ a. List a -> Maybe a
 secondLast = undefined
 
 -- | Drop the N first elements of a list, returning only the remainder. If N
 -- | is higher than the amount of elements in the list, just return the empty
 -- | list. If N is a negative number, drop nothing.
-drop :: forall a. Int -> List a -> List a
+drop :: ∀ a. Int -> List a -> List a
 drop = undefined
 
 -- | Drop elements from the start of a list until a predicate P no longer holds
 -- | true.
 -- | `dropWhile (_ < 9) (1..10) = (9 : 10 : Nil)`
-dropWhile :: forall a. (a -> Boolean) -> List a -> List a
+dropWhile :: ∀ a. (a -> Boolean) -> List a -> List a
 dropWhile = undefined
 
 -- | Find the N'th element of a list, maybe returning a result depending on
 -- | whether or not there are N or more elements in the list. If N is a
 -- | negative number, or bigger than the length of the list, return `Nothing`.
-nth :: forall a. Int -> List a -> Maybe a
+nth :: ∀ a. Int -> List a -> Maybe a
 nth = undefined
 
 -- | Execute a function from `a` to `b`, returning the list where this function
 -- | is executed on each item in the list in order.
-map :: forall a b. (a -> b) -> List a -> List b
+map :: ∀ a b. (a -> b) -> List a -> List b
 map = undefined
 
 -- | Sum the numbers in a list.
@@ -90,47 +90,47 @@ sum :: List Int -> Int
 sum = undefined
 
 -- | Find the number of elements of a list.
-length :: forall a. List a -> Int
+length :: ∀ a. List a -> Int
 length = undefined
 
 -- | Append a list XS to a list YS and return a new list where all the elements
 -- | of XS are immediately followed by the entire list YS.
-append :: forall a. List a -> List a -> List a
+append :: ∀ a. List a -> List a -> List a
 append = undefined
 
 -- | Reverse a list.
-reverse :: forall a. List a -> List a
+reverse :: ∀ a. List a -> List a
 reverse = undefined
 
 -- | Return a list with the same element A repeated N times.
 -- | ```
 -- | replicate 3 "ha" = ("ha" : "ha" : "ha" : Nil)
 -- | ```
-replicate :: forall a. Int -> a -> List a
+replicate :: ∀ a. Int -> a -> List a
 replicate = undefined
 
-foldr :: forall a b. (a -> b -> b) -> b -> List a -> b
+foldr :: ∀ a b. (a -> b -> b) -> b -> List a -> b
 foldr = undefined
 
-foldl :: forall a b. (b -> a -> b) -> b -> List a -> b
+foldl :: ∀ a b. (b -> a -> b) -> b -> List a -> b
 foldl = undefined
 
 -- | Find out whether or not a list is a palindrome.
-isPalindrome :: forall a. Eq a => List a -> Boolean
+isPalindrome :: ∀ a. Eq a => List a -> Boolean
 isPalindrome = undefined
 
 -- | Flatten a list.
-flatten :: forall a. List (List a) -> List a
+flatten :: ∀ a. List (List a) -> List a
 flatten = undefined
 
 -- | Remove consecutive duplicates in a list.
-compress :: forall a. List a -> List a
+compress :: ∀ a. List a -> List a
 compress = undefined
 
 -- | Duplicate the elements in a list.
-duplicate :: forall a. List a -> List a
+duplicate :: ∀ a. List a -> List a
 duplicate = undefined
 
 -- | Duplicate the elements of a list N times.
-duplicateN :: forall a. Int -> List a -> List a
+duplicateN :: ∀ a. Int -> List a -> List a
 duplicateN = undefined
