2eb9e78997793eedbab40fad3c36da9c191a6de4
Rickard Andersson <gonz@severnatazvezda.com>
2020-04-11 19:42:18 +0300

    add `totalAge` & `biggestAgeDifference`

--- src/FpIntro/Exercises/Exercise1.purs
+++ src/FpIntro/Exercises/Exercise1.purs
@@ -19,20 +19,29 @@ newtype Person =
 
 -- Write a function that gets an age and a list of people and returns all the
 -- people above or equal to that age.
-aboveAge :: Int -> List Person
+aboveAge :: Int -> List Person -> List Person
 aboveAge = undefined
 
 -- Write a function that gets an age and a list of people and returns all the
 -- people below that age.
-belowAge :: Int -> List Person
+belowAge :: Int -> List Person -> List Person
 belowAge = undefined
 
 -- Write a function that gets a profession and a list of people and returns all
 -- the people who have that profession.
-haveProfession :: Profession -> List Person
+haveProfession :: Profession -> List Person -> List Person
 haveProfession = undefined
 
 -- Write a function that gets a list of professions and a list of people and
 -- returns all the people who have any of those professions.
-haveAnyOfProfessions :: Profession -> List Person
+haveAnyOfProfessions :: Profession -> List Person -> List Person
 haveAnyOfProfessions = undefined
+
+-- Write a function that gets a list of people and adds up all the age.
+totalAge :: List Person -> Int
+totalAge = undefined
+
+-- Write a function that gets a list of people and returns the biggest age
+-- difference that exists among the people.
+biggestAgeDifference :: List Person -> Int
+biggestAgeDifference = undefined
